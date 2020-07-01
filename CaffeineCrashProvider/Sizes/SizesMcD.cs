2a55e1d7ca43127ba416eaa2c864c70e5265adfd
davidmbillie <davidmbillie@gmail.com>
2020-06-30 20:46:11 -0400

    McD sizes

--- CaffeineCrashProvider/Sizes/SizesMcD.cs
+++ CaffeineCrashProvider/Sizes/SizesMcD.cs
@@ -5,11 +5,28 @@ namespace CaffeineCrashProvider.Sizes
 {
     public class SizesMcD
     {
-        public static readonly Dictionary<string, Beverage> Sizes = new Dictionary<string, Beverage>()
+        //Espresso shots are 71 mg
+
+        public static readonly Dictionary<string, Beverage> Regular = new Dictionary<string, Beverage>()
         {
             {"Small", new Beverage(12, 109)},
             {"Medium", new Beverage(16, 145)},
             {"Large", new Beverage(21, 185)},
+        };
+
+        //same as iced sizes
+        public static readonly Dictionary<string, Beverage> EspressoMilk = new Dictionary<string, Beverage>()
+        {
+            {"Small", new Beverage(12, 71)},
+            {"Medium", new Beverage(16, 142)},
+            {"Large", new Beverage(21, 178)},
+        };
+
+        public static readonly Dictionary<string, Beverage> Iced = new Dictionary<string, Beverage>()
+        {
+            {"Small", new Beverage(16, 133)},
+            {"Medium", new Beverage(21, 200)},
+            {"Large", new Beverage(32, 320)},
         };
     }
 }
