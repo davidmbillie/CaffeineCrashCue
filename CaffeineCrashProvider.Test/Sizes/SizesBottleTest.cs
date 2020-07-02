b78cd703065333c7ad44dac073b746c2b1d9ce92
davidmbillie <davidmbillie@gmail.com>
2020-07-02 14:40:28 -0400

    4/5 sizes test classes

---
+++ CaffeineCrashProvider.Test/Sizes/SizesBottleTest.cs
@@ -0,0 +1,21 @@
+﻿using CaffeineCrashProvider.Models;
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace CaffeineCrashProvider.Sizes
+{
+    public class SizesBottleTest
+    {
+        [Fact]
+        public void CoffeeSizes_CreateReasonableTimes()
+        {
+            foreach (KeyValuePair<string, Beverage> kvp in SizesBottle.Coffee)
+            {
+                double result = Formulas.CalculateCrash(1.0, kvp.Value.Caffeine);
+                Assert.True(result >= 3, kvp.Key + "crash time was too low: " + result.ToString());
+                Assert.True(result <= 6, kvp.Key + "crash time too high: " + result.ToString());
+            }
+        }
+    }
+}
