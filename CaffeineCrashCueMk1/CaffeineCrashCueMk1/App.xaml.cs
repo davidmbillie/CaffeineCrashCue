7442c4151c8fca43f716f0684541c565988e58a1
davidmbillie <davidmbillie@gmail.com>
2020-09-16 22:45:45 -0400

    Fix navigation for Android

--- CaffeineCrashCueMk1/CaffeineCrashCueMk1/App.xaml.cs
+++ CaffeineCrashCueMk1/CaffeineCrashCueMk1/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CaffeineCrashCueMk1
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
