f8f380cf25ffc127ba465b548100a9f228f39d28
davidmbillie <davidmbillie@gmail.com>
2020-09-16 22:54:16 -0400

    SizesSetTest for Reflection usage

---
+++ CaffeineCrashProvider.Test/SizesSetTest.cs
@@ -0,0 +1,22 @@
+﻿using CaffeineCrashProvider.Models;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace CaffeineCrashProvider.Sizes
+{
+	public class SizesSetTest
+	{
+		[Fact]
+		public void SizeDictionariesCanBeLoadedWithReflection()
+		{
+			SizesSet sizesSet = new SizesFastFood();
+			Type fastFoodType = sizesSet.GetType();
+			FieldInfo regularSizeInfo = fastFoodType.GetField("Regular");
+			Dictionary<string, Beverage> result = (Dictionary<string, Beverage>)regularSizeInfo.GetValue(sizesSet);
+			Assert.True(result.ContainsKey("Small"));
+		}
+
+	}
+}
