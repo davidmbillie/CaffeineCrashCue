6c40afa1a5fa6dadda3e2790bbb01e30cd613297
davidmbillie <davidmbillie@gmail.com>
2020-10-18 18:13:02 -0400

    usings cleanup

--- CaffeineCrashCueMk1/CaffeineCrashCueMk1/ConfigPage.xaml.cs
+++ CaffeineCrashCueMk1/CaffeineCrashCueMk1/ConfigPage.xaml.cs
@@ -1,11 +1,5 @@
 ﻿using CaffeineCrashProvider;
-
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
