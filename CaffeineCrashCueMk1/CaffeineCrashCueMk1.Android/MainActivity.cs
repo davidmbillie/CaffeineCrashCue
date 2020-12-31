c441d9b51acd7b1e30a937fe224be376a866de60
davidmbillie <davidmbillie@gmail.com>
2020-12-30 21:22:37 -0500

    Application icon

--- CaffeineCrashCueMk1/CaffeineCrashCueMk1.Android/MainActivity.cs
+++ CaffeineCrashCueMk1/CaffeineCrashCueMk1.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CaffeineCrashCueMk1.Droid
 {
-    [Activity(Label = "CaffeineCrashCueMk1", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CaffeineCrashCueMk1", Icon = "@drawable/caffIcon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
