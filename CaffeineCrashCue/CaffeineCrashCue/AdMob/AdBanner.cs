428ff4afb35456a3b2b7d889d1eb627df5b5398b
davidmbillie <davidmbillie@gmail.com>
2021-10-24 21:15:25 -0400

    Implement ads on Main Page

---
+++ CaffeineCrashCue/CaffeineCrashCue/AdMob/AdBanner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AdMob.CustomRenders
+{
+    public class AdBanner : View
+    {
+        public enum Sizes { Standardbanner, LargeBanner, MediumRectangle, FullBanner, Leaderboard, SmartBannerPortrait }
+        public Sizes Size { get; set; }
+        public AdBanner()
+        {
+            this.BackgroundColor = Color.Accent;
+        }
+    }
+}
