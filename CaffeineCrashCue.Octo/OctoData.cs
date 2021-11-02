9801cb8e43a00619a19455b590fefe18709c562f
davidmbillie <davidmbillie@gmail.com>
2021-11-02 14:57:36 -0400

    OctoData WIP

---
+++ CaffeineCrashCue.Octo/OctoData.cs
@@ -0,0 +1,38 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaffeineCrashCue.Octo
+{
+	public class OctoData
+	{
+		GitHubClient client;
+
+		public OctoData()
+		{
+			ProductHeaderValue productHeaderValue = new ProductHeaderValue("CaffeineCrashCue");
+			client = new GitHubClient(productHeaderValue);
+
+		}
+
+		public async Task GetChecksData()
+		{
+			try
+			{
+				IChecksClient checksClient = client.Check;
+				ICheckRunsClient checkRunsClient = checksClient.Run;
+				CheckRunsResponse runsResponse = await checkRunsClient.GetAllForReference("davidmbillie", "CaffeineCrashCue", "android-with-reports");
+				IReadOnlyList<CheckRun> checkRuns = runsResponse.CheckRuns;
+				CheckRun firstCheckRun = checkRuns[0];
+				CheckRunOutputResponse checkRunOutputResponse = firstCheckRun.Output;
+				string outputText = checkRunOutputResponse.Text;
+			}
+			catch (Exception ex)
+			{
+				string msg = ex.Message;
+			}
+		}
+	}
+}
---
+++ CaffeineCrashCue.Octo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CaffeineCrashCue.Octo
+{
+	class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			OctoData octoData = new OctoData();
+			await octoData.GetChecksData();
+			Console.ReadKey();
+		}
+	}
+}
