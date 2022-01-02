b4addced4b375c46da158d96d949866cb48f628d
davidmbillie <davidmbillie@gmail.com>
2022-01-02 15:48:03 -0500

    Update soda sizes

--- CaffeineCrashProvider/Sizes/SizesSoda.cs
+++ CaffeineCrashProvider/Sizes/SizesSoda.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 
 namespace CaffeineCrashProvider.Sizes
 {
-    /// <summary>
-    /// WIP - might implement in the future
-    /// </summary>
     public class SizesSoda : SizesSet
     {
         public override Dictionary<string, double> QuantOnly
@@ -27,43 +24,43 @@ namespace CaffeineCrashProvider.Sizes
 
         public static readonly Dictionary<string, Beverage> Cola = new Dictionary<string, Beverage>()
         {
-            { "Can/12_oz", new Beverage(12, 3.17)},
-            { "16 oz", new Beverage(16, 3.17)},
-            { "Bottle/20_oz", new Beverage(20, 3.17)},
-            { "30_oz", new Beverage(30, 3.17)},
-            { "40_oz", new Beverage(40, 3.17)},
-            { "44_oz", new Beverage(44, 3.17)}
+            { "Can/Small", new Beverage(12, 3.17)},
+            { "Small", new Beverage(16, 3.17)},
+            { "Bottle/Medium", new Beverage(20, 3.17)},
+            { "Medium/Large", new Beverage(30, 3.17)},
+            { "Large/X-Large", new Beverage(40, 3.17)},
+            { "X-Large", new Beverage(44, 3.17)}
 
         };
 
         public static readonly Dictionary<string, Beverage> Diet_Cola = new Dictionary<string, Beverage>()
         {
-            { "Can/12_oz", new Beverage(12, 3.0)},
-            { "16 oz", new Beverage(16, 3.0)},
-            { "Bottle/20_oz", new Beverage(20, 3.0)},
-            { "30_oz", new Beverage(30, 3.0)},
-            { "40_oz", new Beverage(40, 3.0)},
-            { "44_oz", new Beverage(44, 3.0)}
+            { "Can/Small", new Beverage(12, 3.0)},
+            { "Small", new Beverage(16, 3.0)},
+            { "Bottle/Medium", new Beverage(20, 3.0)},
+            { "Medium/Large", new Beverage(30, 3.0)},
+            { "Large/X-Large", new Beverage(40, 3.0)},
+            { "X-Large", new Beverage(44, 3.0)}
         };
 
         public static readonly Dictionary<string, Beverage> Mtn_Dew = new Dictionary<string, Beverage>()
         {
-            { "Can/12_oz", new Beverage(12, 4.5)},
-            { "16 oz", new Beverage(16, 4.5)},
-            { "Bottle/20_oz", new Beverage(20, 4.5)},
-            { "30_oz", new Beverage(30, 4.5)},
-            { "40_oz", new Beverage(40, 4.5)},
-            { "44_oz", new Beverage(44, 4.5)}
+            { "Can/Small", new Beverage(12, 4.5)},
+            { "Small", new Beverage(16, 4.5)},
+            { "Bottle/Medium", new Beverage(20, 4.5)},
+            { "Medium/Large", new Beverage(30, 4.5)},
+            { "Large/X-Large", new Beverage(40, 4.5)},
+            { "X-Large", new Beverage(44, 4.5)}
         };
 
         public static readonly Dictionary<string, Beverage> Mtn_Dew_High_Caff = new Dictionary<string, Beverage>()
         {
-            { "Can/12_oz", new Beverage(12, 5.62)},
-            { "16 oz", new Beverage(16, 5.62)},
-            { "Bottle/20_oz", new Beverage(20, 5.62)},
-            { "30_oz", new Beverage(30, 5.62)},
-            { "40_oz", new Beverage(40, 5.62)},
-            { "44_oz", new Beverage(44, 5.62)}
+            { "Can/Small", new Beverage(12, 5.62)},
+            { "Small", new Beverage(16, 5.62)},
+            { "Bottle/Medium", new Beverage(20, 5.62)},
+            { "Medium/Large", new Beverage(30, 5.62)},
+            { "Large/X-Large", new Beverage(40, 5.62)},
+            { "X-Large", new Beverage(44, 5.62)}
         };
     }
 }
