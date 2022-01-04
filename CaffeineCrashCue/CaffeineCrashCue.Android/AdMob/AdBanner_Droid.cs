82b112fe98adaf0dd6f203f70b57fa754f1d2215
davidmbillie <davidmbillie@gmail.com>
2022-01-03 22:29:53 -0500

    Swap out test ads

--- CaffeineCrashCue/CaffeineCrashCue.Android/AdMob/AdBanner_Droid.cs
+++ CaffeineCrashCue/CaffeineCrashCue.Android/AdMob/AdBanner_Droid.cs
@@ -45,8 +45,8 @@ namespace AdMob.Droid.CustomRenders
                         adView.AdSize = AdSize.Banner;
                         break;
                 }
-                // TODO: change this id to your admob id  
-                adView.AdUnitId = CueConstants.TestBannerId;
+                
+                adView.AdUnitId = CueConstants.BannerId;
                 var requestbuilder = new AdRequest.Builder();
                 adView.LoadAd(requestbuilder.Build());
                 SetNativeControl(adView);
