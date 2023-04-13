b6a43dbc8ba5309d95fcf4917d820cd92a3c1978
David Billie <davidmbillie@gmail.com>
2023-04-13 12:54:26 -0400

    More automation ids and POMs

--- e2e/CaffeineCrashCueE2E/CaffeineCrashCue.POMs/MainActivity.cs
+++ e2e/CaffeineCrashCueE2E/CaffeineCrashCue.POMs/MainActivity.cs
@@ -2,8 +2,9 @@
 {
     public class MainActivity
     {
-        AndroidDriver<AndroidElement> _driver;
-        AndroidElement CoffeeButton => _driver.FindElementByAccessibilityId("btnCoffee");
+        private AndroidDriver<AndroidElement> _driver;
+        private AndroidElement CoffeeButton => _driver.FindElementByAccessibilityId("btnCoffee");
+        private AndroidElement CustomButton => _driver.FindElementByAccessibilityId("btnCustom");
 
         public MainActivity(AndroidDriver<AndroidElement> driver)
         {
@@ -14,6 +15,12 @@
         {
             CoffeeButton.Click();
             return new ChooseTypePage(_driver);
+        }
+
+        public CustomAmountPage GoToCustomPage()
+        {
+            CustomButton.Click();
+            return new CustomAmountPage(_driver);
         }
     }
 }
---
+++ e2e/CaffeineCrashCueE2E/CaffeineCrashCue.POMs/TimePage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaffeineCrashCue.POMs
+{
+    public class TimePage
+    {
+        private AndroidDriver<AndroidElement> _driver;
+        private AndroidElement NotficationButton => _driver.FindElementByAccessibilityId("btnNotif");
+
+        public TimePage(AndroidDriver<AndroidElement> driver)
+        {
+            _driver = driver;
+        }
+
+        public void SetNotification()
+        {
+            NotficationButton.Click();
+        }
+    }
+}
