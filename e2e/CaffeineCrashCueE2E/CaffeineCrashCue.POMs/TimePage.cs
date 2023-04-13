dad9428eb113119b3f04cb8664a2fb4bc43a54be
David Billie <davidmbillie@gmail.com>
2023-04-13 13:33:17 -0400

    Working local notification smoke test

--- e2e/CaffeineCrashCueE2E/CaffeineCrashCue.POMs/TimePage.cs
+++ e2e/CaffeineCrashCueE2E/CaffeineCrashCue.POMs/TimePage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace CaffeineCrashCue.POMs
     {
         private AndroidDriver<AndroidElement> _driver;
         private AndroidElement NotficationButton => _driver.FindElementByAccessibilityId("btnNotif");
+        private ReadOnlyCollection<AndroidElement> Notifications => _driver.FindElementsById("android:id/title");
 
         public TimePage(AndroidDriver<AndroidElement> driver)
         {
@@ -19,6 +21,27 @@ namespace CaffeineCrashCue.POMs
         public void SetNotification()
         {
             NotficationButton.Click();
+        }
+
+        public void AcceptNotificationAlert()
+        {
+            IAlert alert = _driver.SwitchTo().Alert();
+            alert.Accept();
+        }
+
+        public bool ReceivedCrashNotification()
+        {
+            _driver.OpenNotifications();
+            foreach (AndroidElement notification in Notifications)
+            {
+                if (notification.Text.Contains("Crash"))
+                {
+                    _driver.PressKeyCode(AndroidKeyCode.Back);
+                    return true;
+                }
+            }
+            _driver.PressKeyCode(AndroidKeyCode.Back);
+            return false;
         }
     }
 }
