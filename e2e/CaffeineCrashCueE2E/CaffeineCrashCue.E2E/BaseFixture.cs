dad5d85f299f173cebba84572642775ce7f7a799
David Billie <davidmbillie@gmail.com>
2023-04-17 22:44:09 -0400

    Condense fixtures into configurable base fixture

--- e2e/CaffeineCrashCueE2E/CaffeineCrashCue.E2E/BaseFixture.cs
+++ e2e/CaffeineCrashCueE2E/CaffeineCrashCue.E2E/BaseFixture.cs
@@ -14,11 +14,14 @@ using System.Threading.Tasks;
 namespace CaffeineCrashCue.E2E
 {
     [TestFixture]
-    public class SauceFixture
+    public class BaseFixture
     {
         protected AndroidDriver<AndroidElement> Driver { get; private set; }
 
-        private string sauceUri;
+        private string appiumUri = "";
+        private bool isSauce;
+        private string deviceName;
+        private string platformVersion;
 
         private const string sauceUsWestDomain = "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
 
@@ -28,24 +31,36 @@ namespace CaffeineCrashCue.E2E
             IConfigurationBuilder configBuilder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true)
                 .AddEnvironmentVariables()
-                .AddUserSecrets<SauceFixture>();
+                .AddUserSecrets<BaseFixture>();
 
             IConfigurationRoot config = configBuilder.Build();
 
-            string sauceUser = config.GetRequiredSection("SAUCE_USERNAME").Value ?? throw new ArgumentNullException("");
-            string sauceAccessKey = config.GetRequiredSection("SAUCE_ACCESS_KEY").Value ?? throw new ArgumentNullException("");
+            isSauce = config.GetValue<bool>("IS_SAUCE");
 
-            sauceUri = $"https://{sauceUser}:{sauceAccessKey}{sauceUsWestDomain}";
+            if (isSauce)
+            {
+                string sauceUser = config.GetRequiredSection("SAUCE_USERNAME").Value ?? throw new ArgumentNullException("SAUCE_USERNAME env var is missing");
+                string sauceAccessKey = config.GetRequiredSection("SAUCE_ACCESS_KEY").Value ?? throw new ArgumentNullException("SAUCE_ACCESS_KEY env var is missing");
+                appiumUri = $"https://{sauceUser}:{sauceAccessKey}{sauceUsWestDomain}";
+            }
+            else
+            {
+                appiumUri = "http://localhost:4723/wd/hub";
+            }
+
+            deviceName = config.GetRequiredSection("DEVICE_NAME").Value ?? throw new ArgumentNullException("DEVICE_NAME env var is missing");
+            platformVersion = config.GetRequiredSection("PLATFORM_VERSION").Value ?? throw new ArgumentNullException("PLATFORM_VERSION env var is missing");
+
         }
 
         [SetUp]
         public void Setup()
         {
             var capabilities = new AppiumOptions();
-            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Google Pixel 3a XL GoogleAPI Emulator");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, deviceName);
             // capabilities.AddAdditionalCapability(MobileCapabilityType.AppiumVersion, "1.19.1");
             capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
-            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "11.0");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, platformVersion);
             capabilities.AddAdditionalCapability(AndroidMobileCapabilityType.AppPackage, "com.davidmbillie.CaffeineCrashCue");
             capabilities.AddAdditionalCapability(AndroidMobileCapabilityType.AppActivity, "crc645da8613883ab8f9f.SplashActivity");
             // capabilities.AddAdditionalCapability("appWaitActivity", "crc645da8613883ab8f9f.SplashActivity");
@@ -53,11 +68,14 @@ namespace CaffeineCrashCue.E2E
             capabilities.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name);
             capabilities.AddAdditionalCapability("newCommandTimeout", 320);
 
-            capabilities.AddAdditionalCapability("app",
-                "storage:filename=com.davidmbillie.CaffeineCrashCue.apk");
+            if (isSauce)
+            {
+                capabilities.AddAdditionalCapability("app",
+                    "storage:filename=com.davidmbillie.CaffeineCrashCue.apk");
+            }
 
             //60 seconds is the default for the connection timeout
-            Driver = new AndroidDriver<AndroidElement>(new Uri(sauceUri), capabilities, TimeSpan.FromSeconds(320));
+            Driver = new AndroidDriver<AndroidElement>(new Uri(appiumUri), capabilities, TimeSpan.FromSeconds(320));
         }
 
         [TearDown]
@@ -65,8 +83,11 @@ namespace CaffeineCrashCue.E2E
         {
             if (Driver != null)
             {
-                var isTestPassed = TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed;
-                ((IJavaScriptExecutor)Driver).ExecuteScript("sauce:job-result=" + (isTestPassed ? "passed" : "failed"));
+                if (isSauce)
+                {
+                    var isTestPassed = TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed;
+                    ((IJavaScriptExecutor)Driver).ExecuteScript("sauce:job-result=" + (isTestPassed ? "passed" : "failed"));
+                }
                 Driver.Quit();
                 Driver.Dispose();
             }
