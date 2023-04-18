2927ac68249d482aa401fdb853a5cfc18c9bfe36
David Billie <davidmbillie@gmail.com>
2023-04-18 10:08:42 -0400

    Rest UIAutomator script

---
+++ e2e/CaffeineCrashCueE2E/ResetUIAutomator.bat
@@ -0,0 +1,4 @@
+@REM I had to run these when my local Appium server started throwing socket exceptions
+@REM Run them via the adb command prompt in Android/Tools in Visual Studio
+adb uninstall io.appium.uiautomator2.server
+adb uninstall io.appium.uiautomator2.server.test
